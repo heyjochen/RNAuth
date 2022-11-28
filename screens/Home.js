192e59671b72a738a504966334b0e9f57ae30f4c
Jochen Stierberger <jochen@stierberger.com>
2022-11-28 08:03:25 -0600

    finish auth

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { auth } from '../firebase';
+
 const Home = () => {
+  const navigation = useNavigation();
+
+  const handleLogout = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.replace('Login');
+      })
+      .catch((e) => console.error(e.message));
+  };
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text>{auth.currentUser?.email}</Text>
+      <Pressable onPress={handleLogout} style={styles.button}>
+        <Text style={styles.buttonText}>Logout</Text>
+      </Pressable>
     </View>
   );
 };
 export default Home;
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#0782F9',
+    width: '60%',
+    padding: 15,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+  },
+});
